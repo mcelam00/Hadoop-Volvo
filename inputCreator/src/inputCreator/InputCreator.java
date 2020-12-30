de6897cd15ec5963ca0c9651674034b1eae3e4f7
mcelam00 <mcelam00@mcelam00-vmware>
2020-12-30 18:49:40 +0100

    Base program working creating 2 registers on the file

--- inputCreator/src/inputCreator/InputCreator.java
+++ inputCreator/src/inputCreator/InputCreator.java
@@ -109,6 +109,7 @@ public class InputCreator {
 			/*BEGGINING OF THE PRINTING*/
 			
 			conventionaletElectricRecord(pw); 
+			pw.println();
 			hybridRecord(pw);
 			
 			
@@ -150,10 +151,25 @@ public class InputCreator {
 
 		String type = "";
 		
-		pw.print(randomCity()+"/"+type+"/"+randomDate()+"/"+randomHps()+"/"+randomKMs());
+		int num = (int) Math.floor(Math.random()*2);  //random number between 0 and 1
 		
+		switch(num) { //depending on the random number one of the options will be chosen.
 		
+			case 0:
+				type = "C"; //conventional car			
+				break;
+			case 1:
+				type = "E"; //electric car
+				break;
+						
+		}
 		
+		//number of cars that this family unit (represented by this record) has
+		
+		int cars = (int) ((Math.random() * (2 - 1)) + 1); //1 or 2 cars
+
+		pw.print(randomCity()+"/"+type+"/"+randomDate()+"/"+randomHps(cars)+"/"+randomKMs(cars));
+				
 	}
 
 	/**
@@ -161,14 +177,23 @@ public class InputCreator {
 	 * If 2 cars are to be written there would be 2 kms values separated by an & symbol.
 	 */
 	
-	private String randomKMs() {
-
+	private String randomKMs(int cars) {
 		
+		String km = "";
 		
+		if(cars == 2) {
+			int km1 = (int) Math.floor(Math.random()*500000);  //random number between 0 and 999999
+			int km2 = (int) Math.floor(Math.random()*500000);  //random number between 0 and 999999
+			
+			km = String.valueOf(km1)+"&"+String.valueOf(km2);
 		
+		}else {
 		
+			int km1 = (int) Math.floor(Math.random()*500000);  //random number between 60 and 300
+			km = String.valueOf(km1);
+		}
 		
-		return null;
+		return km;
 	}
 
 	/**
@@ -176,30 +201,86 @@ public class InputCreator {
 	 * If 2 cars are to be written they would be separated by an @ symbol to simplify the map-reduce technique (1 pair key+value).
 	 */
 	
-	private String randomHps() {
+	private String randomHps(int cars) {
 		
 		String hps = "";
 		
-		
-		
-		
+		if(cars == 2) {
+			
+			int h1 = (int) ((Math.random() * (300 - 60)) + 60);  //random number between 60 and 300
+			int h2 = (int) ((Math.random() * (300 - 60)) + 60);  //random number between 60 and 300
+			
+			
+			hps = String.valueOf(h1)+"@"+String.valueOf(h2);
+			
+		}else {
+			
+			int num = (int) ((Math.random() * (300 - 60)) + 60);  //random number between 60 and 300
+
+			hps = String.valueOf(num);
+			
+		}
+			
 		return hps;
 	}
 
 	/**
-	 * Method that generates a random date between 01 January 1970 and 01 January 2021
+	 * Method that generates a random date between January 1990 and January 2021
 	 */
 	
 	private String randomDate() {
 
 		int year;
 		int month;
-		int day;
+		String monthS = "";
 		String date = "";
 		
+		year = (int) ((Math.random() * (2021 - 1990)) + 1990);;  //random number between 1990 and 2021 both inclusive 
 		
+		month = (int) Math.floor(Math.random()*13);  //random number between 0 and 12
 		
+		switch(month) { //depending on the random number one of the options will be chosen.
 		
+			case 0:
+				monthS = "Jan";			
+				break;
+			case 1:
+				monthS = "Feb";
+				break;
+			case 2:
+				monthS = "Mar";
+				break;
+			case 3:
+				monthS = "Aprl";
+				break;
+			case 4:
+				monthS = "May";
+				break;
+			case 5:
+				monthS = "Jun";
+				break;
+			case 6:
+				monthS = "Jul";
+				break;
+			case 7:
+				monthS = "Aug";
+				break;
+			case 8:
+				monthS = "Sep";
+				break;
+			case 9:
+				monthS = "Oct";
+				break;
+			case 10:
+				monthS = "Nov";
+				break;
+			case 11:
+				monthS = "Dec";
+				break;
+		
+		}
+		
+		date = monthS+String.valueOf(year);
 		
 		
 		return date;
@@ -214,12 +295,22 @@ public class InputCreator {
 
 		String city = "";
 		
+		int num = (int) Math.floor(Math.random()*3);  //random number between 0 and 2
 		
+		switch(num) { //depending on the random number one of the options will be chosen.
 		
-		
-		
-		
-		
+			case 0:
+				city = "Uppsala";			
+				break;
+			case 1:
+				city = "Stockholm";
+				break;
+			case 2:
+				city = "Gothenburg";
+				break;
+				
+		}
+				
 		return city;
 	}
 
@@ -232,10 +323,13 @@ public class InputCreator {
 
 		String type = "H";
 		
-		pw.print(randomCity()+"/"+type+"/"+randomDate()+"/"+randomHybHps()+"/"+randomKMs());
 		
-				
+		//number of cars that this family unit (represented by this record) has
 		
+		int cars = (int) ((Math.random() * (2 - 1)) + 1);	
+		
+		pw.print(randomCity()+"/"+type+"/"+randomDate()+"/"+randomHybHps(cars)+"/"+randomKMs(cars));
+
 		
 	}
 	
@@ -243,15 +337,31 @@ public class InputCreator {
 	 * Method that generates a random number of cars (1 or 2) and a value of HP for each of them taking into account that hybrid cars have 2 engines (i.e. 2 hps).
 	 * If 2 cars are to be written they would be separated by an @ symbol to simplify the map-reduce technique (1 pair key+value).
 	 * As they are hyprid and have 2 hps the 2 values would be concatenated using a + sign.
+	 * @param cars 
 	 */
 
-	private String randomHybHps() {
+	private String randomHybHps(int cars) {
 		
 		String hps = "";
 		
-		
-		
-		
+		if(cars == 2) {
+			
+			int h1 = (int) ((Math.random() * (300 - 60)) + 60);  //random number between 60 and 300
+			int h1_1 = (int) ((Math.random() * (100 - 6)) + 6);  //random number between 1 and 100
+			int h2 = (int) ((Math.random() * (300 - 60)) + 60);  //random number between 60 and 300
+			int h2_1 = (int) ((Math.random() * (100 - 6)) + 6);  //random number between 1 and 100
+			
+			hps = String.valueOf(h1)+"+"+String.valueOf(h1_1)+"@"+String.valueOf(h2)+"+"+String.valueOf(h2_1);
+			
+		}else {
+			
+			int num = (int) ((Math.random() * (300 - 60)) + 60);  //random number between 60 and 300
+			int num_1 = (int) ((Math.random() * (100 - 6)) + 6);  //random number between 1 and 100
+
+			hps = String.valueOf(num)+"+"+String.valueOf(num_1);
+			
+		}
+			
 		return hps;
 	}
 	
