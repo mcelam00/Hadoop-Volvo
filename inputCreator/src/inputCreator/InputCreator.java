8552db50e8e2d50b0057a3ef95d76282fab35e1b
mcelam00 <mcelam00@mcelam00-vmware>
2020-12-30 15:02:36 +0100

    Structured the path selector method

--- inputCreator/src/inputCreator/InputCreator.java
+++ inputCreator/src/inputCreator/InputCreator.java
@@ -7,6 +7,8 @@ package inputCreator;
 
 
 public class InputCreator {
+	
+	String filePath = "";
 
 	public static void main(String[] args) {
 		
@@ -21,11 +23,67 @@ public class InputCreator {
 		
 	}
 	
+	/**
+	 * Method that brings in a dialog for us to selct the path in which we want to save the input file
+	 **/
+	 
+	private void savePath() {
+		
+		final JFileChooser fc = new JFileChooser(); //dialog box creation
+		
+		
+		
+		int returnVal = fc.showSaveDialog(null); //show it
+		
+		
+		if(returnVal == JFileChooser.APPROVE_OPTION){  //if we click on accept
+
+			File file = fc.getSelectedFile();
+			
+			//we save the path plus the file name we wish to include
+			filePath = file.getAbsolutePath(); 
+			
+			//append of the .txt extension just in case
+			if((filePath.contains(".txt")) == false) {
+			
+				filePath = filePath +".txt";
+			
+			}
+			
+		}else { //If we interrupt the saving proccess
+			
+			return; 
+			
+		}
+		
+		
+		File f = new File(filePath); //Verification of an existing equally-named file
+		
+		if(f.exists()) { //if it exists
+		
+			int respuestaBoton = JOptionPane.showConfirmDialog(null, "Warning. A file with the same name already exists.\n Do you want to replace it?"); //showconfirm dialog es el tipo de ventana que sale
+		
+			
+			if(respuestaBoton == 0) { //if the replace is wished
+				
+				//CALL TO THE FILEWRITTER
+				
+			}else { //abort
+				
+				return;
+				
+			}
+			
+		}else { //if it does not exist
+			
+			//CALL TO THE FILEWRITTER
+
+		}
 	
+		
+	}
 	
-	
-	
-	
+		
 	
 
 }
