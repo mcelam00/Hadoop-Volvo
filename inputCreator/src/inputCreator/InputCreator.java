35a1f873397dc4bbcec45ae0635f6e79345a6a6d
mcelam00 <47633255+mcelam00@users.noreply.github.com>
2020-12-30 14:37:40 +0100

    Initial commit
    Skeleton of the program

---
+++ inputCreator/src/inputCreator/InputCreator.java
@@ -0,0 +1,31 @@
+package inputCreator;
+
+/**
+ * Class in charge of the creation of an input file following the specifications provided
+ * @author mcelam00
+ */
+
+
+public class InputCreator {
+
+	public static void main(String[] args) {
+		
+			System.out.println("Welcome to the Input creator, now, the generation of the input file will start, this could take some minutes");
+
+		
+			
+		
+	
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
