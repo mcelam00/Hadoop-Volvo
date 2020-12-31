992706e8d385ad92d2376a56df0ec6fe70d6f140
mcelam00 <mcelam00@mcelam00-vmware>
2020-12-31 19:25:58 +0100

    Parser and Mapper classes finished

--- hadoopVolvoSurvey/src/main/java/CarDataRecordParser.java
+++ hadoopVolvoSurvey/src/main/java/CarDataRecordParser.java
@@ -2,40 +2,272 @@
 import org.apache.hadoop.io.Text;
 
 public class CarDataRecordParser {
-  
-  private static final int MISSING_TEMPERATURE = 9999;
-  
-  private String year;
-  private int airTemperature;
-  private String quality;
-  
+ 
+	 private String city;	 //would be the key
+	 private String type[];	//since we could have 2 cars I represent is as an array, if only one is full the other would not even exist
+	 private String buildDate[]; 
+	 private String horsePower[];
+	 private String kilometres[];
+	 private int numOfCars; //just to keep info of the number of cars using the size of the arrays
+	
   public void parse(String record) {
-    year = record.substring(15, 19);
-    String airTemperatureString;
-    // Remove leading plus sign as parseInt doesn't like them (pre-Java 7)
-    if (record.charAt(87) == '+') { 
-      airTemperatureString = record.substring(88, 92);
-    } else {
-      airTemperatureString = record.substring(87, 92);
-    }
-    airTemperature = Integer.parseInt(airTemperatureString);
-    quality = record.substring(92, 93);
+    
+	  /*HERE WE HAVE 1 LINE OF THE FILE IN STRING FORMAT*/
+	  
+	  //We are going to split the fields by the "/" so that we can separate each of them
+	  
+	  String [] holeRecord;
+	  
+	  holeRecord = record.split("/"); //in this way we achieve, in each position of the array one record 0 would be the city; 1 the type; 2 the dates...
+	  
+	  this.city = holeRecord[0];
+	  
+		  if(areThere2Cars() == true) {	//If there are two cars, both array positions would be created
+			  
+			  	this.numOfCars = 2; //set the atribute first of all for later methods
+			 
+			  /**TYPES**/
+				 this.type = holeRecord[1].split("+"); //holeRecord[1] would be the String representing the types concatenated by a +; so, we split them and save them likewise we did with the "/"
+			  
+			  /**BUILD DATES**/
+				 this.buildDate = holeRecord[2].split("@");
+			  
+			  /**HORSE POWERS**/
+				 this.horsePower = holeRecord[3].split("@");
+			  
+			  /**KILOMETRES**/
+				 this.kilometres	= holeRecord[4].split("&");
+			  
+		  }else { //whereas if there is only one car, just an array of 1 would be created
+			  
+			  	this.numOfCars = 1; //set the atribute first of all for later methods
+
+			  
+			  /*ARRAY CREATION*/
+			  	 this.type = new String[1];
+				 this.buildDate = new String[1]; 
+				 this.horsePower = new String[1];
+				 this.kilometres = new String[1];
+			  
+			  /**TYPE**/
+				 this.type[0] = holeRecord[1];
+			  
+			  /**BUILD DATE**/
+				 this.buildDate[0] = holeRecord[2];
+			  
+			  /**HORSE POWERS**/
+				 this.horsePower[0] = holeRecord[3];
+			  
+			  /**KILOMETRES**/
+				 this.kilometres[0] = holeRecord[4];
+			 			  
+		  }
+	  
+	  
+	   
   }
+  
+  
+  			/*MISCELLANY*/
+  
+  /**
+   * Parse from Text hadooop format to String format
+   * @param record
+   */
   
   public void parse(Text record) {
     parse(record.toString());
   }
 
-  public boolean isValidTemperature() {
-    return airTemperature != MISSING_TEMPERATURE && quality.matches("[01459]");
+  /**
+   * It looks for an @ all over the record of the horsspowers and if it finds it, that would mean 2 cars
+   * @return true if there are 2 cars, false in case just one
+   */
+  
+  private boolean areThere2Cars() {
+	  
+	  boolean flag = false;
+	  
+	  for (int i = 0, i < this.horsepowers.length(); i++){ 
+		  
+		  char c = this.horsepowers.charAt(i); //I take char per char and look for the @ 
+		  
+		  if(c == '@') {
+			  flag = true;
+			  break;
+		  }
+					  
+	  }
+	  	  
+	  return flag;
+	  	  
   }
   
-  public String getYear() {
-    return year;
+  
+  
+  			/*VALIDATIONS AND VERIFICATIONS*/
+  
+  /*RECORD FIELDS VALIDATION*/
+	    
+  
+  public boolean isValidRecord() {
+	
+	  boolean flag = true; //by default is valid unless --- is found in any field
+	  
+	  if(this.numOfCars == 1) {
+		  
+		  if(this.type[0].charAt(0) == '-' || this.buildDate[0].charAt(0) == '-' || this.horsePower[0].charAt(0) == '-' || this.kilometres[0].charAt(0) == '-') { //invalid field
+			  flag = false;
+		  }
+		  
+	  }else { //2cars, so we have 2 Strings in each array, so we look the first character of each of both strings to see if it si - meaning that the second and third chars would be -- also constituting the invalid field
+		  
+		  if(this.type[0].charAt(0) == '-' || this.buildDate[0].charAt(0) == '-' || this.horsePower[0].charAt(0) == '-' || this.kilometres[0].charAt(0) == '-') { //invalid field
+			  flag = false;
+		  }else if (this.type[1].charAt(0) == '-' || this.buildDate[1].charAt(0) == '-' || this.horsePower[1].charAt(0) == '-' || this.kilometres[1].charAt(0) == '-') {
+			  flag = false;
+		  }
+	  
+	  }
+	  
+	return flag;	  
+	  
+  }
+  
+  
+  /*GETTER NUMBER OF CARS*/
+  
+  
+  public int numOfCars() {
+	  return this.numOfCars;
   }
 
-  public int getAirTemperature() {
-    return airTemperature;
+  
+  /*GETTER TYPE OF CAR 1*/
+  
+  
+  public String getTypeCar1() {
+	  return this.type[0];
   }
+  
+  
+  /*GETTER TYPE OF CAR 2*/
+  
+  
+  public String getTypeCar2() {
+	  return this.type[1];
+  }
+  
+ 
+  /*CAR AGE VALIDATION*/
+
+  
+  public boolean isAfter2000(String type) {
+	  //Depending on the car to validate one validation or the other
+  
+	  boolean flag = true;
+	  
+	  if(type.compareTo(this.type[0]) == 0) { //is vehicle 1
+		  
+		  if(this.buildDate[0].charAt(3) != '2') { //Character in pos 3 is the first of the year if its not 2 not valid
+		  
+			  flag = false;
+			  
+		  }
+		  
+		  
+	  }else { //is vehicle 2
+		  
+		  
+		  if(this.buildDate[1].charAt(3) != '2') { //Character in pos 3 is the first of the year if its not 2 not valid
+			  
+			  flag = false;
+			  
+		  }
+		  
+		  
+		  
+	  }
+  
+  
+	  return flag;
+  
+  
+  }
+  
+  
+  /*CAR KMS VALIDATION*/
+
+  
+  public boolean isLessThan450000(String type) {
+	  
+	 boolean flag = true;
+
+	  if(type.compareTo(this.type[0]) == 0) { //is vehicle 1
+		  
+		  if(Integer.parseInt(this.kilometres[0]) > 450000) { //more than 450 000 not valid record
+			  flag = false;
+  
+		  }
+		  
+		  
+	  }else { //is vehicle 2
+		  
+			  
+			if(Integer.parseInt(this.kilometres[1]) > 450000) { //more than 450 000 not valid record
+				  flag = false;
+		  
+			}
+		  
+	  }
+	  
+  
+	  return flag;
+  
+  
+  }
+  
+  
+  /*COMPUTES THE EFFECTIVE POWER OF THE HYBRID ENGINE Car 1*/
+
+  
+  public int hyEffectivePowerCar1() {
+	  
+	  String power[] = this.horsePower[0].split("+");
+	  	
+	  int pw1 = Integer.parseInt(power[0]);
+	  int pw2 = Integer.parseInt(power[1]);
+	  
+	  return (pw1+pw2);	  
+  }
+  
+  
+  /*COMPUTES THE EFFECTIVE POWER OF THE HYBRID ENGINE Car 2*/
+  
+public int hyEffectivePowerCar2() {
+	  
+	  String power[] = this.horsePower[1].split("+");
+	  	
+	  int pw1 = Integer.parseInt(power[0]);
+	  int pw2 = Integer.parseInt(power[1]);
+	  
+	  return (pw1+pw2);	  
+  }
+  
+  
+  
+  			/*GETTING THE KEY AND THE VALUE TO THE MAPPER*/
+  
+  public String getCity() { //key
+    return this.city;
+  }
+
+  public String getHorsePowerCar1() { //value
+    return this.horsePower[0];
+  }
+  public String getHorsePowerCar2() { //value'
+	    return this.horsePower[1];
+	  }
+  
 }
 
--- hadoopVolvoSurvey/src/main/java/volvoSurveyMapper.java
+++ hadoopVolvoSurvey/src/main/java/volvoSurveyMapper.java
@@ -7,43 +7,259 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class volvoSurveyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+public class volvoSurveyMapper extends Mapper<LongWritable, Text, Text, IntWritable> { //habria que poner text tmb
 
   private CarDataRecordParser parser = new CarDataRecordParser();
 
   @Override
   public void map(LongWritable key, Text value, Context context)throws IOException, InterruptedException {
 
-    
-	  /*CAR AGE VALIDATION*/
-	  boolean age = 
-			  
-	  /*CAR TYPE VALIDATION*/
-			  
-	  /*ERRORS IN SOME FIELD OF THE RECORDS*/
-	  
-	  
-	  /**/
-	  
-	  
-	  
-	  
-	  
-	  
+	  /*SEPARATING RECORD PARTS*/
 	  
 	  parser.parse(value);
-    if (parser.isValidTemperature()) {//si en el registro la temperatura es correcta y la calidad es buena escribe en el contexto
-      context.write(new Text(parser.getYear()),new IntWritable(parser.getAirTemperature())); //debe pasar la clave valor sobre los que opera el reducer, pero el reducer no piensa ni sabe los buenos/malos
-    }
+	  
+	  /*ERRORS IN SOME FIELD OF THE RECORDS*/
+	  
+	  if(parser.isValidRecord() == false) {
+		  
+		  
+		  /*It is declined completely due to considering it unrealiable*/
+		  
+		  
+	  /*NO ERRORS 1 CAR PER THIS RECORD*/
+		  
+	  }else if(parser.numOfCars() == 1) {
+		  
+		  	/*If it is not electric, ok*/
+		  if(parser.getTypeCar1().compareTo("E") != 0) {
+			  
+			  /*It has to have less than 450.000km and be newer than 2000*/
+			  if(parser.isAfter2000(parser.getTypeCar1()) == true && parser.isLessThan450000(parser.getTypeCar1()) == true) {
+				 
+				  //Precondition: Convetional or hybrids with this satisfyed conditions
+				  
+				  if(parser.getTypeCar1().compareTo("C") == 0) { //if Conventional: we write in the context
+					  
+					  context.write(new Text(parser.getCity()),new IntWritable(Integer.parseInt(parser.getHorsePowerCar1())));
+					  
+					  
+				  }else if(parser.getTypeCar1().compareTo("H") == 0) { //if Hybrid: we calculate the effective power and write in the context
+					  
+					 					  
+					  context.write(new Text(parser.getCity()),new IntWritable(parser.hyEffectivePowerCar1()));
+					  
+					  					  
+				  }				  
+				  				  
+			  }			  
+			  
+		  }
+	  
+		  
+	  /*NO ERRORS 2 CARS PER THIS RECORD*/
+		  
+	  }else {
+		  
+		  if(parser.getTypeCar1().compareTo("E") == 0 && parser.getTypeCar2().compareTo("E") == 0) {
+			  
+			  /*both are electric == record declined*/
+		  
+			  
+		  }else {
+			  //means, one is electric, the other is or none
+			  
+			  if(parser.getTypeCar1().compareTo("E") == 0) {//if one is electric we write the other in the context
+				  
+				  /*It has to have less than 450.000km and be newer than 2000*/
+				  if(parser.isAfter2000(parser.getTypeCar2()) == true && parser.isLessThan450000(parser.getTypeCar2()) == true) {
+					  //Since no elect+hyb is conventional
+					  
+					  context.write(new Text(parser.getCity()),new IntWritable(Integer.parseInt(parser.getHorsePowerCar2())));
+
+				  }
+				  
+				  
+			  }else if(parser.getTypeCar2().compareTo("E") == 0) { //if the other is electric we do the other way round
+				  			  
+				  
+				  if(parser.isAfter2000(parser.getTypeCar1()) == true && parser.isLessThan450000(parser.getTypeCar1()) == true) {
+					  //Since no hybrid+electric is conventional
+					  
+					  context.write(new Text(parser.getCity()),new IntWritable(Integer.parseInt(parser.getHorsePowerCar1())));
+
+				  }
+				  
+				  
+				  
+				  
+			  }else {
+				  
+				  //or the 2 conventional or the two hybrids (no persons in the survey with Hybrid and conventional)
+				  
+				  if(parser.getTypeCar1().compareTo("C") == 0 && parser.getTypeCar2().compareTo("C") == 0) {
+					  
+					  //We need to check if both of them satisfy the age and kms
+					  
+					  if(parser.isAfter2000(parser.getTypeCar1()) == true) { //first has good age
+						  if(parser.isLessThan450000(parser.getTypeCar1()) == true) { //first has good kms
+							  	//first is ok
+							  if(parser.isAfter2000(parser.getTypeCar2()) == true) { //second has good age
+								  if(parser.isLessThan450000(parser.getTypeCar2()) == true) { //second has good kms
+									  	//second is ok and we pass the mean to the reducer
+							  
+									  	/*We take both cars powers and do the mean*/
+									  int pw1 = Integer.parseInt(parser.getHorsePowerCar1());
+									  int pw2 = Integer.parseInt(parser.getHorsePowerCar2());
+									  
+									  int mean = (pw1+pw2)/2
+									  
+									  context.write(new Text(parser.getCity()),new IntWritable(mean));
+
+									  
+									  
+									  
+								  }else {//second has not good kms (declined)
+									   
+									  //pasa el 1
+									  
+									  
+									  context.write(new Text(parser.getCity()),new IntWritable(Integer.parseInt(parser.getHorsePowerCar1())));
+
+									  
+									  
+									  
+								  }
+							  
+							  }else { //second has not good age (declined)
+								  
+								  //pasa el 1
+								  
+								  
+								  context.write(new Text(parser.getCity()),new IntWritable(Integer.parseInt(parser.getHorsePowerCar1())));
+
+								  
+								  
+							  }
+							  
+						  }else{ //first has not good kms (declined)
+							  
+							  //second is ok?
+							  
+							  if(parser.isAfter2000(parser.getTypeCar2()) == true && parser.isLessThan450000(parser.getTypeCar2()) == true) { 
+								  	//yes
+								  
+								  context.write(new Text(parser.getCity()),new IntWritable(Integer.parseInt(parser.getHorsePowerCar2())));
+ 
+								  
+								  
+							  }
+							  
+							  
+						  }
+
+					  }else { //first not good age (declined)
+						  
+						//second is ok?
+						  
+						  if(parser.isAfter2000(parser.getTypeCar2()) == true && parser.isLessThan450000(parser.getTypeCar2()) == true) { 
+							  	//yes
+							  
+							  context.write(new Text(parser.getCity()),new IntWritable(Integer.parseInt(parser.getHorsePowerCar2())));
+ 
+							  
+							  
+						  }
+						  
+					  }
+					  
+					  
+				  }else if(parser.getTypeCar1().compareTo("H") == 0 && parser.getTypeCar2().compareTo("H") == 0) {
+					  
+					//We need to check if both of them satisfy the age and kms
+					  
+					  if(parser.isAfter2000(parser.getTypeCar1()) == true) { //first has good age
+						  if(parser.isLessThan450000(parser.getTypeCar1()) == true) { //first has good kms
+							  	//first is ok
+							  if(parser.isAfter2000(parser.getTypeCar2()) == true) { //second has good age
+								  if(parser.isLessThan450000(parser.getTypeCar2()) == true) { //second has good kms
+									  	//second is ok and we pass the mean to the reducer
+							  
+									  	/*We take both cars powers and do the mean*/
+									  int pw1 = Integer.parseInt(parser.hyEffectivePowerCar1());
+									  int pw2 = Integer.parseInt(parser.hyEffectivePowerCar2());
+									  
+									  int mean = (pw1+pw2)/2
+									  
+									  context.write(new Text(parser.getCity()),new IntWritable(mean));
+
+									  
+									  
+									  
+								  }else {//second has not good kms (declined)
+									   
+									  //pasa el 1
+									  
+									  
+									  context.write(new Text(parser.getCity()),new IntWritable(Integer.parseInt(parser.hyEffectivePowerCar1())));
+
+									  
+									  
+									  
+								  }
+							  
+							  }else { //second has not good age (declined)
+								  
+								  //pasa el 1
+								  
+								  
+								  context.write(new Text(parser.getCity()),new IntWritable(Integer.parseInt(parser.hyEffectivePowerCar1())));
+
+								  
+								  
+							  }
+							  
+						  }else{ //first has not good kms (declined)
+							  
+							  //second is ok?
+							  
+							  if(parser.isAfter2000(parser.getTypeCar2()) == true && parser.isLessThan450000(parser.getTypeCar2()) == true) { 
+								  	//yes
+								  
+								  context.write(new Text(parser.getCity()),new IntWritable(Integer.parseInt(parser.hyEffectivePowerCar2())));
+ 
+								  
+								  
+							  }
+							  
+							  
+						  }
+
+					  }else { //first not good age (declined)
+						  
+						//second is ok?
+						  
+						  if(parser.isAfter2000(parser.getTypeCar2()) == true && parser.isLessThan450000(parser.getTypeCar2()) == true) { 
+							  	//yes
+							  
+							  context.write(new Text(parser.getCity()),new IntWritable(Integer.parseInt(parser.hyEffectivePowerCar2())));
+ 
+							  
+							  
+						  }
+						  
+					  } 
+					  
+					  
+				  }
+				 
+			  }			  
+			  
+		  }			  
+		  
+		 
+	  }
   
-  
-  
-  
-  
-  
-  
-  
-  
+	
   }
 }
 
